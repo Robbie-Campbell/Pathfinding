9570f4524b98b5eefda582e5b490af90bfe71598
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-09-15 13:57:57 +0100

    :sparkles: added a frog NPC :sparkles:

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed = 0.00001f;
+
+    public GameObject continueButton;
+    public GameObject DestroyableNPC;
+    public GameObject enemySpawner;
+    public Animator animator;
+    public AudioSource audio;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    void Update()
+    {
+        if(textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+       foreach(char letter in sentences[index].ToCharArray())
+        {
+            audio.Play();
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+
+    public void NextSentence()
+    {
+        animator.SetTrigger("Change");
+        continueButton.SetActive(false);
+        if (index < sentences.Length - 1)
+        {
+            index += 1;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            continueButton.SetActive(false);
+            Time.timeScale = 1;
+            Destroy(DestroyableNPC);
+            enemySpawner.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/TalkToFrog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TalkToFrog : MonoBehaviour
+{
+    public GameObject dialogManager;
+    // Start is called before the first frame update
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Player")
+        {
+            dialogManager.SetActive(true);
+            Time.timeScale = 0.001f;
+        }
+    }
+}
